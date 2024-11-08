ef87640ab3a3ee3c0eb913a5ee26e48a165f9551
harantsuki <anyazosen@gmail.com>
2024-11-08 15:19:14 +0700

    third commit

--- src/constant/project.js
+++ src/constant/project.js
@@ -4,21 +4,24 @@ const PROJECT_LIST = [
     title: "Valorant",
     desc: "I remade the Valorant website with a dynamic, responsive design showcasing trailers and agent info",
     buttonTitle: "For More",
-    url: "https://harantsuki.github.io/Remake-Valorant/",
+    url: "https://github.com/harantsuki/Remake-Valorant",
+    // url: "https://harantsuki.github.io/Remake-Valorant/",
   },
   {
     img: "./img-1.png",
     title: "Notion",
     desc: "I remade Notion with a minimalist design, drag-and-drop features, and real-time collaboration.",
     buttonTitle: "For More",
-    url: "https://harantsuki.github.io/Remake-Notion/",
+    url: "https://github.com/harantsuki/Remake-Notion",
+    // url: "https://harantsuki.github.io/Remake-Notion/",
   },
   {
     img: "./img-10.png",
     title: "Nike",
     desc: "I remade the Nike website with a responsive e-commerce design and product search features.",
     buttonTitle: "For More",
-    url: "https://harantsuki.github.io/Remake-Nike/",
+    url: "https://github.com/harantsuki/Remake-Nike",
+    // url: "https://harantsuki.github.io/Remake-Nike/",
   },
 ];
 
